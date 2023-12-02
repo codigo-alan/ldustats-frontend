f709c3f81f9e933b1d4980b77d1303f49512b197
Alan <alanfmarcos@gmail.com>
2023-12-02 11:31:30 +0100

    Solve the undefined problem at render historical info

--- client/src/components/historicalInfoPlayerComponent/HistoricalInfoPlayerComponent.jsx
+++ client/src/components/historicalInfoPlayerComponent/HistoricalInfoPlayerComponent.jsx
@@ -50,97 +50,20 @@ export function HistoricalInfoPlayerComponent({playerRef}) {
 
 
     return (
-        
-        <form className="gap-2">
-            <div>
-            <div className="col card bg-light p-3 d-inline-block">
-                <p className="fw-bold text-end">Velocidad: {historicalInfo.velocity}</p>
-                <p className="fw-bold text-end">Distancia: {historicalInfo.distance}</p>
-                <p className="fw-bold text-end">Sprints: {historicalInfo.sprints}</p>
-                <p className="fw-bold text-end">Sprints Dist.: {historicalInfo.sprintsDistance}</p>
-                <p className="fw-bold text-end">Aceleraciones: {historicalInfo.accelerations}</p>
-                <p className="fw-bold text-end">Desaceleraciones: {historicalInfo.decelerations}</p>
-            </div>
+            
+        <div >
+            {historicalInfo &&
+                <div className="col card bg-light p-3 d-inline-block">
+                    <p className="fw-bold text-end">Velocidad: {historicalInfo.velocity}</p>
+                    <p className="fw-bold text-end">Distancia: {historicalInfo.distance}</p>
+                    <p className="fw-bold text-end">Sprints: {historicalInfo.sprints}</p>
+                    <p className="fw-bold text-end">Sprints Dist.: {historicalInfo.sprintsDistance}</p>
+                    <p className="fw-bold text-end">Aceleraciones: {historicalInfo.accelerations}</p>
+                    <p className="fw-bold text-end">Desaceleraciones: {historicalInfo.decelerations}</p>
+
+                </div>}
+            
         </div>
-            {/* <div className="row">
-                <div className="card gap-2 p-2 bg-light">
-                    <div className="form-group row">
-                        <label className="col-7 col-form-label">Velocidad:</label>
-                        <div className="col-5">
-                            <input
-                                type="text"
-                                placeholder="0"
-                                className="form-control"
-                                readOnly={true}
-                                {...register('velMax')}
-                            />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-7 col-form-label">Distancia:</label>
-                        <div className="col-5">
-                            <input
-                                type="text"
-                                placeholder="0"
-                                className="form-control"
-                                readOnly={true}
-                                {...register('totalDistance')}
-                            />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-7 col-form-label">Sprints:</label>
-                        <div className="col-5">
-                            <input
-                                type="text"
-                                placeholder="0"
-                                className="form-control"
-                                readOnly={true}
-                                {...register('sprints')}
-                            />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-7 col-form-label">Distancia Sprints:</label>
-                        <div className="col-5">
-                            <input
-                                type="text"
-                                placeholder="0"
-                                className="form-control"
-                                readOnly={true}
-                                {...register('sprintsDistance')}
-                            />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-7 col-form-label">Aceleraciones:</label>
-                        <div className="col-5">
-                            <input
-                                type="text"
-                                placeholder="0"
-                                className="form-control"
-                                readOnly={true}
-                                {...register('maxAcc')}
-                            />
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <label className="col-7 col-form-label">Desaceleraciones:</label>
-                        <div className="col-5">
-                            <input
-                                type="text"
-                                placeholder="0"
-                                className="form-control"
-                                readOnly={true}
-                                {...register('maxDec')}
-                            />
-                        </div>
-                    </div>
 
-                </div>
-
-            </div> */}
-
-        </form>
     )
 }
