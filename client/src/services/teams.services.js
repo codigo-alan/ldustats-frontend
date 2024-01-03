d9365e1f7aa445a153066729526d550e177ab4b6
Alan Marcos <amarcos@click-it.es>
2024-01-03 19:59:05 +0100

    Fix route of prod back

--- client/src/services/teams.services.js
+++ client/src/services/teams.services.js
@@ -3,7 +3,7 @@ import { addInterceptors } from "../utils/Interceptors";
 
 //create a base URL to avoid repeat the url string
 const teamsApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000/ldustats/api/v1/teams/'
+    baseURL: 'https://ldustats-backend-production.up.railway.app/ldustats/api/v1/teams/'
 })
 
 addInterceptors(teamsApi);
